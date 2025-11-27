c5ebb4f748a5a2bc59f71f269c1eb3d5aa963387
jhDrech <jdrechmer@gmail.com>
2025-11-27 17:01:37 -0300

    27/11 Elias Cardoso e João Drechmer

---
+++ model/Tabela.java
@@ -0,0 +1,10 @@
+package model;
+import java.util.HashMap;
+
+public class Tabela {
+    HashMap<Integer, Metal> t = new HashMap<>();
+    
+    public void acharMetal(){
+
+    }
+}
---
+++ view/MainCLI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainCLI{
+    public void telaInicial(){
+        while (true) {
+            System.out.println("ESCOLHA UM METAL:");
+            System.out.println("1:|Cu| 2:|Ag| 3:|Fe| 4:|Ni| 5:|Mg| 6:|Al| 7:|Zn| 8:|Cr| 9:|Pb| 10:|Co|");
+            int metal1 = sc.nextInt();
+            System.out.println(); 
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
